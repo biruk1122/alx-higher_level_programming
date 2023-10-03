10e23dad889f61154b75d989a5ceadcaec810050
biruk1122 <birukfikadu29@gmail.com>
2023-10-03 13:27:59 -0700

    updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) not in ( 'q' , 'e' ):
-        print("{}".format(chr(letter)) , end="")
+    if chr(letter) not in ('q', 'e'):
+        print("{}".format(chr(letter)), end="")
