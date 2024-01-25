5278266020279fba6ed443c214471c98001149d2
biruk1122 <birukfikadu29@gmail.com>
2024-01-25 12:00:48 -0800

    upd

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make a request to 0.0.0.0:5000/catch_me using curl
-response=$(curl -s -X PUT -L 0.0.0.0:5000/catch_me -d "user_id=98")
+curl -s -X PUT -L 0.0.0.0:5000/catch_me -d "user_id=98" > /dev/null
