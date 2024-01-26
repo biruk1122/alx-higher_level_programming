9e06b0add4af8d759d2f7ddd1056a40c49f04785
biruk1122 <birukfikadu29@gmail.com>
2024-01-26 10:45:28 -0800

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status using the requests package.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
