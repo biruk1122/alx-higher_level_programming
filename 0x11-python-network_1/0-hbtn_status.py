db46a9756c61c9fd03039af30e19748e0219693c
biruk1122 <birukfikadu29@gmail.com>
2024-01-26 10:30:41 -0800

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 
-try:
+if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type:", type(html))
         print("\t- content:", html.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code:", e.code)
-except urllib.error.URLError as e:
-    print("Error:", e.reason)
